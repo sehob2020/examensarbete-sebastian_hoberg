ce829bc8a35b4406974a1908f9527c3e6c64aa8d
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2022-02-07 15:16:25 +0100

    Fixing circle color

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,17 @@ import { Button } from '@chakra-ui/button';
 import { Circle, Stack, Flex, Box, Text, Link } from '@chakra-ui/layout';
 import { useMediaQuery } from '@chakra-ui/media-query';
 import React from 'react';
+import { useColorMode } from '@chakra-ui/color-mode';
 
 function Header() {
 
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+    const { colorMode, toggleColorMode } = useColorMode();
+    const isDark = colorMode === "dark";
 
     return (
         <Stack>
-            <Circle position="absolute" bg="blue.800" opacity="0.1"
-                w="300px" h="300px" alignSelf="flex-end" />
+            <Circle position="absolute" bg={isDark ? "blue.800" : "blue.200"} opacity="0.1" w="300px" h="300px" alignSelf="flex-end" />
             <Flex direction={isNotSmallerScreen ? "row" : "column"}
                 spacing="200px" p={isNotSmallerScreen ? "32" : "0"} 
                 alignSelf="flex-start">
