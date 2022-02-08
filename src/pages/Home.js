487e3660146b2d9387a8f86cebdb860f33efa13e
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2022-02-08 15:23:49 +0100

    creation of component techs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import { HStack } from '@chakra-ui/layout';
-import { motion } from "framer-motion"
+
 import Social from '../components/Social';
 
 function Home() {
   return (
       <HStack display="flex" alignItems="center" justifyContent="center" h="full" w="100%">
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
           <Social />
-        </motion.div>  
       </HStack>
   );
 }
